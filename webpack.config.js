65d27eed0de5830c784a88d573fd660ec54b8dae
Prashant Chaudhari <prashantchaudhari881@gmail.com>
2019-04-08 17:32:02 +0530

    add output details and loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,18 @@
+const path = require('path');
+
 module.exports = {
-  entry: './src/index.js'
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname,'dist'),
+    filename: "main.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ],
+  },
 }
