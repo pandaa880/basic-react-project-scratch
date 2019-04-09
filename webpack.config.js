6a7e74f630ab082d7976175bb05a71ca8af5c939
Prashant Chaudhari <prashantchaudhari881@gmail.com>
2019-04-09 10:51:05 +0530

    add mode property and add configuration for htmlwebpacplugin and webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,12 @@
 const path = require('path');
 
+// plugins
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+// config imports
+
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname,'dist'),
@@ -11,8 +17,33 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: ['babel-loader']
+        use: [
+          {
+            loader: 'babel-loader'
+          }
+        ]
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader'
+          }
+        ]
       }
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'React Project Setup from scratch',
+      template: path.resolve(__dirname, 'public/index.html'),
+      filename: "./index.html"
+    })
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+    quiet: true
+  }
 }
