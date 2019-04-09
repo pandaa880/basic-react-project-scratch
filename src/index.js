f88815482d41d94a3e48176d2c44188379bc0d68
Prashant Chaudhari <prashantchaudhari881@gmail.com>
2019-04-09 11:35:15 +0530

    import app component to render

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// relative imports
+import App from './App';
+
 const root = document.getElementById('root');
 
 const SampleApp = () => (
   <div>
     <h1>This is a basic react project created from scratch.</h1>
     <p>Working...</p>
+    <App />
   </div>
 );
 
